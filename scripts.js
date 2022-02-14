38d42b5263c85a57ff0e66b93cdad8fbaf43e7ba
Mauricio-Camilo <mauricio.ecamilo@gmail.com>
2022-02-14 13:56:13 -0300

    Projeto finalizado, porém sem todos os requisitos cumpridos

--- scripts.js
+++ scripts.js
@@ -1,38 +1,34 @@
 let mensagensServidor = [];
 let usuarioEnviado = [];
-let teste = "";
-let mostrarUsuario = "";
+let nomeUsuario = "";
 let usuarioConectado = "";
 let enviarMensagem = "";
 
 function receberNomeUsuario() {
    
-    // while (statusServidor !== 200) {
-    const nomeUsuario = prompt("Digite seu nome para entrar na sala:");
-    const usuario = {name: nomeUsuario};
-  //  console.log(usuario);
+    const entradaUsuario = prompt("Digite seu nome para entrar na sala:");
+    const usuario = {name: entradaUsuario};
     const promise = axios.post("https://mock-api.driven.com.br/api/v4/uol/participants",usuario);
-    // Fazer um loop até aceitar o nome do usuário
     promise.then(enviarNomeCorreto);
-    promise.catch(tratarErro);
+    promise.catch(enviarNomeNovamente);
     usuarioConectado = usuario;
-    mostrarUsuario = usuario.name;
-    console.log (usuario);
-// }
+    nomeUsuario = usuario.name;
 }
 
 function enviarNomeCorreto(nomeEnviado) {
-    teste = nomeEnviado.status;
-    console.log(teste);
-    while (teste !== 200) {
-        prompt ("Nome inválido, digite outro nome:");
-        receberNomeUsuario();
-    }
+    setTimeout(mostrarNomeUsuario, 1000);
 }
 
-function EsconderTelaLogin() {
-    const ocultarLogin = document.querySelector(".login");
-    ocultarLogin.classList.add("escondido")
+function enviarNomeNovamente() {
+    alert ("Nome inválido, digite outro nome");
+    receberNomeUsuario();
+}
+
+function mostrarNomeUsuario() {
+    const mensagem = document.querySelector("main");
+    mensagem.innerHTML += `<div class="entrada-saida exibição">
+    <p> <b> ${nomeUsuario} </b> <span> entra na sala...</span></p>
+</div>`
 }
 
 function carregarMensagensDoServidor() {
@@ -47,7 +43,7 @@ function cumprirPromessa(resposta) {
 }
 
 function tratarErro(erro) {
-    console.log(erro.response.status); //400ja
+    console.log(erro.response.status); 
     console.log(erro.response.data);
 }
 
@@ -55,34 +51,26 @@ function mostrarMensagensDoServidor() {
     const mensagem = document.querySelector("main");
     mensagem.innerHTML = "";
     for (let i = 0; i < mensagensServidor.length; i++) {
-        let texto = mensagensServidor[i];
-        if (texto.type === "status") {
+        let conteudoMensagem = mensagensServidor[i];
+        if (conteudoMensagem.type === "status") {
             mensagem.innerHTML += `<div class="entrada-saida exibição">
-        <p><span class="tempo">(${texto.time})   </span> <b> ${texto.from} </b> <span>  ${texto.text} </span></p>
+        <p><span class="tempo">(${conteudoMensagem.time})   </span> <b> ${conteudoMensagem.from} </b> <span>  ${conteudoMensagem.text} </span></p>
     </div>`
         }
-        else if (texto.type === "message") {
-            mensagem.innerHTML += `<div class="mensagem exibição">
-        <p><span class="tempo">(${texto.time})</span> <b> ${texto.from} </b> <span> para  </span><b>${texto.to}</b>: ${texto.text}</p>
+        else if (conteudoMensagem.type === "message") {
+            mensagem.innerHTML += `<div class="mensagem exibição data-identifier="message"">
+        <p><span class="tempo">(${conteudoMensagem.time})</span> <b> ${conteudoMensagem.from} </b> <span> para  </span><b>${conteudoMensagem.to}</b>: ${conteudoMensagem.text}</p>
     </div>`}
-        else {
+        else if (conteudoMensagem.type === "private-message" && nomeUsuario !== conteudoMensagem.to) {
             mensagem.innerHTML += `<div class="reservada exibição">
-            <p><span class="tempo">(${texto.time})</span> <b> ${texto.from} </b> <span>reservadamente para  </span><b>${texto.to}</b>: ${texto.text}</p>
+            <p><span class="tempo">(${conteudoMensagem.time})</span> <b> ${conteudoMensagem.from} </b> <span>reservadamente para  </span><b>${conteudoMensagem.to}</b>: ${conteudoMensagem.text}</p>
          </div>`}
     }
 }
 
-function mostrarNomeUsuario() {
-    const mensagem = document.querySelector("main");
-    mensagem.innerHTML += `<div class="entrada-saida exibição">
-    <p> <b> ${mostrarUsuario} </b> <span> entra na sala...</span></p>
-</div>`
-}
-
 function manterConexao() {
-    console.log (usuarioConectado);
+    console.log (usuarioConectado); 
     const promise = axios.post("https://mock-api.driven.com.br/api/v4/uol/status",usuarioConectado);
-    // Fazer um loop até aceitar o nome do usuário
     promise.then(verificar5segundos);
     promise.catch(removerUsuario);
 }
@@ -94,34 +82,39 @@ function verificar5segundos() {
 function removerUsuario() {
     const mensagem = document.querySelector("main");
     mensagem.innerHTML += `<div class="entrada-saida exibição">
-    <p> <b> ${mostrarUsuario} </b> <span> sai da sala...</span></p>
+    <p> <b> ${nomeUsuario} </b> <span> sai da sala...</span></p>
 </div>`
-
 }
 
 function digitarMensagem () {
     const mensagemDigitada = document.querySelector(".enviar-mensagem")
     const mensagemEnviada = mensagemDigitada.value;
-    enviarMensagem = {from: mostrarUsuario, to: "Todos", text: mensagemEnviada, type: "message"};
-    console.log (enviarMensagem);
+    enviarMensagem = {from: nomeUsuario, to: "Todos", text: mensagemEnviada, type: "message"};
+    console.log (enviarMensagem); 
     const promise = axios.post("https://mock-api.driven.com.br/api/v4/uol/messages", enviarMensagem);
-    promise.then (mensagemEnviadaOK);
-    promise.catch (tratarErro)
+    promise.then (mensagemEnviadaOk);
+    promise.catch (mensagemNaoEnviada);
 }
 
-function mensagemEnviadaOK () {
-    console.log ("sucesso");
+function mensagemEnviadaOk () {
+    const mensagem = document.querySelector("main");
+    mensagem.innerHTML += `<div class="mensagem exibição">
+        <p><b> ${enviarMensagem.from} </b> <span> para  </span><b>${enviarMensagem.to}</b>: ${enviarMensagem.text}</p>
+    </div>`
+    const resetarMensagem = document.querySelector(".enviar-mensagem")
+    resetarMensagem.value = "";
+    console.log ("envio com sucesso");
+}
+
+function mensagemNaoEnviada() {
+    receberNomeUsuario();
 }
 
  receberNomeUsuario();
 
  carregarMensagensDoServidor();
 
- setTimeout(mostrarNomeUsuario, 1000);
-
  setInterval(carregarMensagensDoServidor, 3000);
 
  setInterval(manterConexao, 5000);
 
- 
- //setInterval(carregarMensagensDoServidor, 2000);
